cf753907c14751f73c261734da81096c696331f2
Mateusz Baczek <matbak7@gmail.com>
2019-06-02 23:47:59 +0200

    added random sleep for comp move

--- kolko_krzyzyk_simple_console.py
+++ kolko_krzyzyk_simple_console.py
@@ -1,10 +1,8 @@
 from time import sleep
 from random import randint, uniform
 
-gracz = 'X'
-komp = 'O'
-POLE = "-"
-plansza = [POLE] * 9
+znaczniki = ('X', 'O', '-')
+plansza = [znaczniki[2]] * 9
 wygrane = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 4, 8), (2, 4, 6), (0, 3, 6), (1, 4, 7), (2, 5, 8))
 win = False
 
@@ -16,7 +14,7 @@ def rysuj_plansze():
 
 
 def check_plansza():
-    if POLE not in plansza:
+    if znaczniki[2] not in plansza:
         return False
     return True
 
@@ -27,62 +25,62 @@ def ruch_gracza():
     while True:
         if check_plansza():
             ruch = int(input("Podaj numer następnego ruchu od 0 do 8: "))
-            if plansza[ruch] == POLE and 0 <= ruch < 9:
-                plansza[ruch] = gracz
+            if plansza[ruch] == znaczniki[2] and 0 <= ruch < 9:
+                plansza[ruch] = znaczniki[0]
                 break
         else:
             print("Błąd spróbój ponownie")
 
 
-def ruch_komputera():
-    print("Ruch komputera")
+def ruch_komutera():
+    print("Ruch znaczniki[1]utera")
     rysuj_plansze()
     set = False
     if check_plansza():
         sleep(uniform(1.2, 4.2))
-        if plansza[4] == POLE:
-            plansza[4] = komp
+        if plansza[4] == znaczniki[2]:
+            plansza[4] = znaczniki[1]
             set = not set
-        if (plansza[4] == gracz and plansza[8] == gracz) or (plansza[1] == gracz and plansza[2] == gracz) or \
-                (plansza[3] == gracz and plansza[6] == gracz) and not set:
-            if plansza[0] == POLE:
-                plansza[0] = komp
+        if (plansza[4] == znaczniki[0] and plansza[8] == znaczniki[0]) or (plansza[1] == znaczniki[0] and plansza[2] == znaczniki[0]) or \
+                (plansza[3] == znaczniki[0] and plansza[6] == znaczniki[0]) and not set:
+            if plansza[0] == znaczniki[2]:
+                plansza[0] = znaczniki[1]
                 set = not set
-        if (plansza[0] == gracz and plansza[2] == gracz) or (plansza[4] == gracz and plansza[7] == gracz) and not set:
-            if plansza[1] == POLE:
-                plansza[1] = komp
+        if (plansza[0] == znaczniki[0] and plansza[2] == znaczniki[0]) or (plansza[4] == znaczniki[0] and plansza[7] == znaczniki[0]) and not set:
+            if plansza[1] == znaczniki[2]:
+                plansza[1] = znaczniki[1]
                 set = not set
-        if (plansza[0] == gracz and plansza[1] == gracz) or (plansza[4] == gracz and plansza[6] == gracz) or \
-                (plansza[5] == gracz and plansza[8] == gracz) and not set:
-            if plansza[2] == POLE:
-                plansza[2] = komp
+        if (plansza[0] == znaczniki[0] and plansza[1] == znaczniki[0]) or (plansza[4] == znaczniki[0] and plansza[6] == znaczniki[0]) or \
+                (plansza[5] == znaczniki[0] and plansza[8] == znaczniki[0]) and not set:
+            if plansza[2] == znaczniki[2]:
+                plansza[2] = znaczniki[1]
                 set = not set
-        if (plansza[0] == gracz and plansza[6] == gracz) or (plansza[4] == gracz and plansza[5] == gracz) and not set:
-            if plansza[3] == POLE:
-                plansza[3] = komp
+        if (plansza[0] == znaczniki[0] and plansza[6] == znaczniki[0]) or (plansza[4] == znaczniki[0] and plansza[5] == znaczniki[0]) and not set:
+            if plansza[3] == znaczniki[2]:
+                plansza[3] = znaczniki[1]
                 set = not set
-        if (plansza[2] == gracz and plansza[8] == gracz) or (plansza[3] == gracz and plansza[4] == gracz) and not set:
-            if plansza[5] == POLE:
-                plansza[5] = komp
+        if (plansza[2] == znaczniki[0] and plansza[8] == znaczniki[0]) or (plansza[3] == znaczniki[0] and plansza[4] == znaczniki[0]) and not set:
+            if plansza[5] == znaczniki[2]:
+                plansza[5] = znaczniki[1]
                 set = not set
-        if (plansza[0] == gracz and plansza[3] == gracz) or (plansza[4] == gracz and plansza[2] == gracz) or \
-                (plansza[7] == gracz and plansza[8] == gracz) and not set:
-            if plansza[6] == POLE:
-                plansza[6] = komp
+        if (plansza[0] == znaczniki[0] and plansza[3] == znaczniki[0]) or (plansza[4] == znaczniki[0] and plansza[2] == znaczniki[0]) or \
+                (plansza[7] == znaczniki[0] and plansza[8] == znaczniki[0]) and not set:
+            if plansza[6] == znaczniki[2]:
+                plansza[6] = znaczniki[1]
                 set = not set
-        if (plansza[6] == gracz and plansza[8] == gracz) or (plansza[4] == gracz and plansza[1] == gracz) and not set:
-            if plansza[8] == POLE:
-                plansza[7] = komp
+        if (plansza[6] == znaczniki[0] and plansza[8] == znaczniki[0]) or (plansza[4] == znaczniki[0] and plansza[1] == znaczniki[0]) and not set:
+            if plansza[8] == znaczniki[2]:
+                plansza[7] = znaczniki[1]
                 set = not set
-        if (plansza[0] == gracz and plansza[4] == gracz) or (plansza[2] == gracz and plansza[5] == gracz) or (plansza[6] == gracz and plansza[7] == gracz) and not set:
-            if plansza[8] == POLE:
-                plansza[8] = komp
+        if (plansza[0] == znaczniki[0] and plansza[4] == znaczniki[0]) or (plansza[2] == znaczniki[0] and plansza[5] == znaczniki[0]) or (plansza[6] == znaczniki[0] and plansza[7] == znaczniki[0]) and not set:
+            if plansza[8] == znaczniki[2]:
+                plansza[8] = znaczniki[1]
                 set = not set
         if not set:
             ruch = randint(0, 8)
-            while plansza[ruch] != POLE:
+            while plansza[ruch] != znaczniki[2]:
                 ruch = randint(0, 8)
-            plansza[ruch] = komp
+            plansza[ruch] = znaczniki[1]
 
 
 def jak_grac():
@@ -107,13 +105,13 @@ def wygrana():
     else:
         for mozliwosc in wygrane:
             if plansza[mozliwosc[0]] == plansza[mozliwosc[1]] == plansza[mozliwosc[2]]:
-                if plansza[mozliwosc[0]] == komp:
+                if plansza[mozliwosc[0]] == znaczniki[1]:
                     win = True
-                    print("WYGRYWA KOMPUTERA")
+                    print("WYGRYWA znaczniki[1]UTERA")
                     rysuj_plansze()
                     sleep(5)
                     return True
-                elif plansza[mozliwosc[0]] == gracz:
+                elif plansza[mozliwosc[0]] == znaczniki[0]:
                     win = True
                     print("WYGRALES!!!")
                     rysuj_plansze()
@@ -126,9 +124,9 @@ def main():
     global win, plansza
     while True:
         win = False
-        plansza = [POLE] * 9
+        plansza = [znaczniki[2]] * 9
         while not win:
-            for _ in (ruch_gracza(), ruch_komputera()):
+            for _ in (ruch_gracza(), ruch_komutera()):
                 if wygrana():
                     break
 
